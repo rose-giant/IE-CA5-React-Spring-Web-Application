ded416967112d17ae3e398daed9dddef0619120f
Fatemeh <f.shahhosseini@gmail.com>
2024-04-27 00:09:57 +0330

    fix: street field added to Address object

--- backend/src/main/java/Mizdooni/Model/Address.java
+++ backend/src/main/java/Mizdooni/Model/Address.java
@@ -3,10 +3,16 @@ package Mizdooni.Model;
 public class Address {
     public String city;
     public String country;
+    public String street;
 
     public Address(String city, String country) {
         this.city = city;
         this.country = country;
+    }
+    public Address(String city, String country, String street) {
+        this.city = city;
+        this.country = country;
+        this.street = street;
     }
 
     public Address(){}
