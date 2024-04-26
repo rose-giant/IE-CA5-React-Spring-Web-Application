e4821710d2fd1b6888a596cdcb3e8767399e85f9
Fatemeh <f.shahhosseini@gmail.com>
2024-04-26 23:10:26 +0330

    fix: redundant codes removed

--- backend/src/main/java/Mizdooni/MizdooniApplication.java
+++ backend/src/main/java/Mizdooni/MizdooniApplication.java
@@ -7,72 +7,9 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.event.EventListener;
 
-//@SpringBootApplication
-//public class MizdooniApplication {
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(MizdooniApplication.class, args);
-//	}
-//
-//}
 @SpringBootApplication
 public class MizdooniApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(MizdooniApplication.class, args);
 	}
 }
-//
-//@SpringBootConfiguration
-//public class MizdooniApplication {
-//
-//	@Bean
-//	MyBean myBean() {
-//		return new MyBean();
-//	}
-//
-//	@Bean
-//	ApplicationRunner appRunner() {
-//		return new MyAppRunner();
-//	}
-//
-//	@Bean
-//	ExitCodeExceptionMapper exitCodeExceptionMapper() {
-//		return exception -> {
-//
-//			if (exception.getCause() instanceof MyException) {
-//				return 10;
-//			}
-//			return 1;
-//		};
-//	}
-//
-//	public static void main(String[] args) {
-//		SpringApplication.run(MizdooniApplication.class, args);
-//	}
-//
-//	private static class MyAppRunner implements ApplicationRunner {
-//
-//		@Override
-//		public void run(ApplicationArguments args) throws Exception {
-//			System.out.println("in command line");
-//			if (true) throw new MyException("test");
-//		}
-//	}
-//
-//	private static class MyBean {
-//
-//		@EventListener
-//		public void exitEvent(ExitCodeEvent event) {
-//			System.out.println("-- ExitCodeEvent --");
-//			System.out.println("exit code: " + event.getExitCode());
-//		}
-//	}
-//
-//	private static class MyException extends RuntimeException {
-//
-//		public MyException(String message) {
-//			super(message);
-//		}
-//
-//	}
-//}
