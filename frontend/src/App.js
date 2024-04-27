9d83f5d685cd38110b561c131558ee16f84b790e
rose-giant <itsrozasworld@gmail.com>
2024-04-27 11:44:26 -0400

    feat: test component added to app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
-import Home from './components/Home/home';
+import InitUser from './components/Auth/init';
+import Footer from './components/Footer/footer';
+import About from './components/Home/about';
+import Hero from './components/Home/hero';
+import RestaurantList from './components/Home/restaurantlist';
+import Nav from './components/Nav/nav';
+import RestaurantDetail from './components/Restaurant/detail';
+import UserCustomer from './pages/Customer';
 
 function App() {
+  const user = {
+    address:{
+      city:"Pittsburgh",
+      country:"US"},
+    email:"ali@gmail.com",
+    password:"ali_1234",
+    role:"manager",
+    username:"ali"
+  }
   return (
     <div className="App">
-      <Home />
+      {/* <RestaurantList/> */}
+      <UserCustomer user={user} />
     </div>
   );
 }
