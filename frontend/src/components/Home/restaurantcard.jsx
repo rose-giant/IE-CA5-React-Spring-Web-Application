1e9303fa6e6cdf0dc96b6d98e759b5db0610c33f
rose-giant <itsrozasworld@gmail.com>
2024-04-27 11:41:15 -0400

    feat: restaurant list and card components added

--- frontend/src/components/Home/restaurantcard.jsx
+++ frontend/src/components/Home/restaurantcard.jsx
@@ -1,14 +1,12 @@
 import React from "react"
 import "./restaurantcard.css"
+import { IsOpen } from "../Helpers/isOpen"
 
-export default function RestaurantCard() {
+export default function RestaurantCard({ restaurant }) {
+        
     return(
         <>
             <div class="card">
-                    <div class="header">
-
-                    </div>
-
                     <div class="body">
                         {/* <div class="pancreas">
                             <img id="star" src="./icons/svg/pinkstar.svg" alt=""/>
@@ -17,29 +15,35 @@ export default function RestaurantCard() {
                             <img id="star" src="./icons/svg/pinkstar.svg" alt=""/>
                             <img id="star" src="./icons/svg/pinkstar.svg" alt=""/>
                         </div> */}
-                        <img src="./cards/rest1.png" alt=""/>
+                        <img src={restaurant.image} alt=""/>
                     </div>
 
                     <div class="card-footer">
                         <p class="title">
-                            The Ivy Brasserie
+                            {restaurant.name}
                         </p>
                         <p class="reviews">
-                            2096 reviews
+                            2096 &nbsp; reviews
                         </p>
-                        <p class="place">
-                            Contemporary British
-
-                        <p class="location">
-                            <img class="icon location-icon" src="./icons/svg/location-pin-svgrepo-com.svg" alt=""/>
-                            Tehran
+                        <p className="type">
+                            {restaurant.type}
                         </p>
+                        <p class="place">                        
+                            <p class="location">
+                                <img class="icon location-icon" src="./icons/location.png" alt=""/>
+                                <p>{restaurant.address.city}</p>
+                            </p>
                         </p>
 
                         <p class="open">
-                            <span class="open-span">Open .</span>
-                            Closes at 11 PM
+                            <span class="open-span">
+                                {IsOpen(restaurant.startTime, restaurant.endTime) ? 
+                                    `Open. Closes at ${restaurant.endTime}` :
+                                    `Closed. Opens at ${restaurant.startTime}`
+                                } &nbsp;&nbsp;&nbsp;
+                            </span>
                         </p>
+
                     </div>
                 </div>
         </>
