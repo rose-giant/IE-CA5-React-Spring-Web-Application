51837f6370b92efe8dd4e8a07d05065cc8c831cf
rose-giant <itsrozasworld@gmail.com>
2024-04-27 11:41:49 -0400

    feat: reviews components added

---
+++ frontend/src/components/Reviews/reviewItem.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+import "./reviews.css"
+
+export default function ReviewItem({review}) {
+    return(
+        <>
+            <div class="left">
+                <div class="profile">
+                    {review.username[0]}
+                </div>
+
+                <div class="comment">
+                    <p class="username">
+                        {review.username}
+                    </p>
+
+                    <ul class="review-detail">
+                        <li>Overall &nbsp;<span>{review.overallRate}</span>.</li>
+                        <li>Food &nbsp;<span>{review.foodRate}</span>.</li>
+                        <li>Service &nbsp;<span>{review.serviceRate}</span>.</li>
+                        <li>Ambiance &nbsp;<span>{review.ambianceRate}</span></li>
+                    </ul>
+
+                    <p class="comment-body">
+                        {review.comment}
+                    </p>
+                </div>
+            </div>
+
+            <div class="right">
+                {/* <div class="stars">
+                    <img class="staricon" src="./../assets/icons/svg/pinkstar.svg" alt="">
+                    <img class="staricon" src="./../assets/icons/svg/pinkstar.svg" alt="">
+                    <img class="staricon" src="./../assets/icons/svg/pinkstar.svg" alt="">
+                    <img class="staricon" src="./../assets/icons/svg/pinkstar.svg" alt="">
+                    <img class="staricon" src="./../assets/icons/svg/star-rate-rating-outline-svgrepo-com.svg" alt="">
+                </div> */}
+
+                <p class="date">
+                    Dined on February 17, 2024
+                </p>
+            </div>
+        </>
+    )
+}
