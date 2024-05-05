1bcb3bec57f179580f9910519c7cda8bd675e78f
rose-giant <itsrozasworld@gmail.com>
2024-05-05 12:56:26 -0400

    fix: href replaced with usenavigate to avoid refreshing

--- frontend/src/components/Nav/nav.jsx
+++ frontend/src/components/Nav/nav.jsx
@@ -2,14 +2,19 @@ import React from "react"
 import { useContext } from "react"
 import "./../Home/home.css"
 import { Context } from "../../App"
+import { useNavigate } from "react-router-dom"
 
 export default function Nav() {
     const [signedIn, setSignedIn] = useContext(Context)
+    const navigate = useNavigate()
+    const handleNavigation = () => {
+        navigate("/")
+    }
 
     return (
         <nav class="navbar px-5 py-2">
             <div class="nav-left">
-                <a class="navbar-brand" href="/">
+                <a class="navbar-brand" onClick={handleNavigation}>
                     <img class="logo" src="./logo/logo.png" alt="" />
 
                     <p class="nav-title">
