59715480342c316710cb8dd95ece59fc99cf8a9c
rose-giant <itsrozasworld@gmail.com>
2024-06-02 14:49:25 -0400

    feat: review controller logging implemented

--- backend/src/main/java/Mizdooni/Controller/ReviewController.java
+++ backend/src/main/java/Mizdooni/Controller/ReviewController.java
@@ -3,6 +3,8 @@ package Mizdooni.Controller;
 
 import Mizdooni.Model.Review.Review;
 import Mizdooni.Model.Review.ReviewRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +16,7 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/reviews")
 public class ReviewController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
     private final ReviewRepository reviewRepo;
     @Autowired
     public ReviewController() throws Exception {
@@ -21,6 +24,7 @@ public class ReviewController {
     }
     @GetMapping("")
     public ArrayList<Review> getAll() {
+        LOGGER.info("get all reviews API requested");
         return reviewRepo.getAll();
     }
 }
