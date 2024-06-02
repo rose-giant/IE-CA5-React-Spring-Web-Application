b6eae663b66df519a2f80872950defac077e29e2
rose-giant <itsrozasworld@gmail.com>
2024-06-02 14:51:06 -0400

    feat: user controller logging implemented

--- backend/src/main/java/Mizdooni/Controller/UserController.java
+++ backend/src/main/java/Mizdooni/Controller/UserController.java
@@ -4,6 +4,8 @@ package Mizdooni.Controller;
 import Mizdooni.Model.User.User;
 import Mizdooni.Model.User.UserRepository;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,6 +17,7 @@ import java.util.Map;
 @RequestMapping("/users")
 public class UserController {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
     private final UserRepository userRepo;
 
     @Autowired
@@ -24,6 +27,7 @@ public class UserController {
 
     @GetMapping("")
     public ArrayList<User> getAll() {
+        LOGGER.info("get all users API requested");
         return userRepo.getAll();
     }
 }
