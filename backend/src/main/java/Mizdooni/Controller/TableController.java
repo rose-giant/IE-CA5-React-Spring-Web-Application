0017f636902400a829670e9880053491637a5cf6
rose-giant <itsrozasworld@gmail.com>
2024-06-02 14:50:19 -0400

    feat: table controller logging implemented

--- backend/src/main/java/Mizdooni/Controller/TableController.java
+++ backend/src/main/java/Mizdooni/Controller/TableController.java
@@ -2,6 +2,8 @@ package Mizdooni.Controller;
 
 import Mizdooni.Model.Table.Table;
 import Mizdooni.Model.Table.TableRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/tables")
 public class TableController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
     private final TableRepository tableRepo;
     @Autowired
     public TableController() throws Exception {
@@ -19,6 +22,7 @@ public class TableController {
     }
     @GetMapping("")
     public ArrayList<Table> getAll() {
+        LOGGER.info("get all tables API requested");
         return tableRepo.getAll();
     }
 }
