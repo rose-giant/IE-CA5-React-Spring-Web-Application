bdc16c1d4af7cf8d9283f4306d23f63164a4f2d5
rose-giant <itsrozasworld@gmail.com>
2024-06-02 14:48:22 -0400

    feat: reservation controller logging implemented

--- backend/src/main/java/Mizdooni/Controller/ReservationController.java
+++ backend/src/main/java/Mizdooni/Controller/ReservationController.java
@@ -2,6 +2,8 @@ package Mizdooni.Controller;
 
 import Mizdooni.Model.Reservation.Reservation;
 import Mizdooni.Model.Reservation.ReservationRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,6 +14,7 @@ import java.util.ArrayList;
 @RestController
 @RequestMapping("/reservations")
 public class ReservationController {
+    private static final Logger LOGGER = LoggerFactory.getLogger(AuthController.class);
     private final ReservationRepository reservationRepo;
     @Autowired
     public ReservationController() throws Exception {
@@ -19,6 +22,7 @@ public class ReservationController {
     }
     @GetMapping("")
     public ArrayList<Reservation> getAll() {
+        LOGGER.info("get all reservations API requested");
         return reservationRepo.getAll();
     }
 }
